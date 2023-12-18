74cfb16475a2dfaa7284c7eb1082bd1c5f9fc662
hyuga <hyuga3110@gmail.com>
2023-12-18 21:11:09 +0900

    support svg

--- next.config.js
+++ next.config.js
@@ -1,9 +1,22 @@
-const {
-    createVanillaExtractPlugin
-  } = require('@vanilla-extract/next-plugin');
-  const withVanillaExtract = createVanillaExtractPlugin();
+const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin')
+const withVanillaExtract = createVanillaExtractPlugin()
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+        },
+      ],
+    })
+    return config
+  },
+  // images: {
+  //   disableStaticImages: true,
+  // },
+}
 
 module.exports = withVanillaExtract(nextConfig)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import styles from './page.module.css'
+import NoteIcon from '../assets/note.svg'
 
 export default function Home() {
   return (
     <main className={styles.container}>
       <div className={styles.hoge}></div>
+      <NoteIcon height={100} width={100}/>
     </main>
   )
 }
